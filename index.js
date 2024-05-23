de7938bafc24c69e42f48e75b34aa0eddfff29e3
ahnaf4d <ahnaftahmid802@gmail.com>
2024-05-23 21:36:54 +0600

    some rest services added

--- index.js
+++ index.js
@@ -33,6 +33,10 @@ async function run() {
       .collection('carts');
 
     // user related apis
+    app.get('/users', async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.status(200).send(result);
+    });
     app.post('/users', async (req, res) => {
       const user = req.body;
       const query = { email: user.email };
@@ -43,6 +47,24 @@ async function run() {
       const result = await userCollection.insertOne(user);
       res.status(200).send(result);
     });
+    app.delete('/users/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    });
+    app.patch('/users/admin/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          role: 'admin',
+        },
+      };
+      const result = await userCollection.updateOne(filter, updatedDoc);
+      res.status(200).send(result);
+    });
+    // menu related apis
     app.get('/menu', async (req, res) => {
       const result = await menuCollection.find().toArray();
       res.status(200).send(result);
