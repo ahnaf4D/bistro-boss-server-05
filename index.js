1ebc8247fc9f3d85753bcd527493436c3b693abb
ahnaf4d <ahnaftahmid802@gmail.com>
2024-05-24 17:46:53 +0600

    final commit

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const app = express();
+
 const port = process.env.PORT || 3000;
 // middlewares
 app.use(express.json());
@@ -32,10 +34,58 @@ async function run() {
       .db('bistroBossRestaurantDB')
       .collection('carts');
 
+    // jwt related apis
+    app.post('/jwt', (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN, {
+        expiresIn: '1h',
+      });
+      res.send({ token });
+    });
+    // MiddleWares
+    const verifyTokens = (req, res, next) => {
+      console.log('Inside the verify token', req.headers.authorization);
+      if (!req.headers.authorization) {
+        return res.status(401).send({ massage: 'unauthorized access' });
+      }
+      const token = req.headers.authorization.split(' ')[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ massage: 'unauthorized access' });
+        }
+        req.decoded = decoded;
+        next();
+      });
+    };
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      const isAdmin = user?.role === 'admin';
+      if (!isAdmin) {
+        return res.status(403).send({ massage: 'For bidden Access' });
+      }
+      next();
+    };
+
     // user related apis
-    app.get('/users', async (req, res) => {
+    app.get('/users', verifyTokens, verifyAdmin, async (req, res) => {
+      console.log('hit on the users');
       const result = await userCollection.find().toArray();
       res.status(200).send(result);
+    });
+    app.get('/users/admin/:email', verifyTokens, async (req, res) => {
+      const email = req.params.email;
+      if (email !== req.decoded.email) {
+        return res.status(403).send({ massage: 'forbidden access' });
+      }
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      let admin = false;
+      if (user) {
+        admin = user?.role === 'admin';
+      }
+      res.send({ admin });
     });
     app.post('/users', async (req, res) => {
       const user = req.body;
